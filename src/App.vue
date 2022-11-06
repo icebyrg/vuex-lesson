e596e6b824c3a51959175fc817cd4d3520085032
icebyrg <shildreturn@outlook.com>
2022-11-06 14:06:57 +0800

    test

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,36 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  计数器：{{ count }}{{ $store.state.count }}
+  <button @click="$store.state.count++">错误修改</button>
+  <button @click="add">同步修改</button>
+  <button @click="asyncAdd">异步修改</button>
+  <hr />
+  a模块：{{ aCount }}{{ bCount }}
+  <button @click="$store.commit('aCount/add', 1)">改a</button>
+  <button @click="$store.commit('bCount/add', 1)">改b</button>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import { computed } from 'vue'
+import { useStore } from '@/vuex'
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
-  }
+  setup() {
+    const store = useStore()
+    function add() {
+      store.commit('add', 1)
+    }
+    function asyncAdd() {
+      store.dispatch('asyncAdd', 1)
+    }
+    return {
+      count: computed(() => store.state.count),
+      double: computed(() => store.getters.double),
+      aCount: computed(() => store.state.aCount.count),
+      bCount: computed(() => store.state.bCount.count),
+      add,
+      asyncAdd,
+    }
+  },
 }
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
--- src/store/index.js
+++ src/store/index.js
@@ -1,14 +1,54 @@
-import { createStore } from 'vuex'
+import { createStore } from '@/vuex'
 
 export default createStore({
   state: {
+    count: 0,
   },
   getters: {
+    double(state) {
+      return state.count * 2
+    },
   },
   mutations: {
+    add(state, payload) {
+      state.count += payload
+    },
   },
   actions: {
+    asyncAdd({ commit }, payload) {
+      setTimeout(() => {
+        commit('add', payload)
+      }, 1000)
+    },
   },
   modules: {
-  }
+    aCount: {
+      namespaced: true,
+      state: { count: 0 },
+      mutations: {
+        add(state, payload) {
+          state.count += payload
+        },
+      },
+      modules: {
+        cCount: {
+          state: { count: 0 },
+          mutations: {
+            add(state, payload) {
+              state.count += payload
+            },
+          },
+        },
+      },
+    },
+    bCount: {
+      namespaced: true,
+      state: { count: 0 },
+      mutations: {
+        add(state, payload) {
+          state.coun += payload
+        },
+      },
+    },
+  },
 })
